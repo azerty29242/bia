3c0863418f4fc595c07d0e733b74dd6e20d50dd0
azerty29242 <94782485+azerty29242@users.noreply.github.com>
2024-04-17 17:50:49 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from "solid-js";
+
+// ?
+// !
+// * Part one
+// TODO:
+const App: Component = () => {
+  return (
+    <div class="container mx-auto px-4">
+      <h1 class="text-3xl font-bold">Révisions pour le BIA</h1>
+    </div>
+  );
+};
+
+export default App;
